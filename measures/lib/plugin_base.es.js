4f8fc45068543c920199f6b1d23c2f7d5e5c30ac
yanlong.yang <yyl_love_you@126.com>
2025-03-07 16:19:45 +0800

    feat: add measure

---
+++ measures/lib/plugin_base.es.js
@@ -0,0 +1,156 @@
+var cyjh_soft =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "./";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 217);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 217:
+/*!*******************************************************!*\
+  !*** ./src/functionality_three/plugins/pluginBase.ts ***!
+  \*******************************************************/
+/*! exports provided: PluginBase */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PluginBase", function() { return PluginBase; });
+/**
+ * PluginBase 插件的基类
+ */
+let pluginID = 0;
+class PluginBase {
+    constructor(viewer, ...args) {
+        /**
+         * 三维容器对象
+         */
+        this.object3d = null;
+        Object.defineProperty(this, 'id', { value: pluginID++ });
+        this.viewer = viewer;
+        // 初始化三维容器
+        this.object3d = new PluginBase.easy3DAPI.THREE.Group();
+    }
+    /**
+     * 初始化插件相关参数
+     * @param options
+     */
+    init(options) { }
+    /**
+     * 更新插件
+     */
+    update(dt) { }
+    /**
+     * 卸载插件
+     */
+    removeSelf() { }
+    /**
+     * 返回插件名称
+     */
+    /**
+     * 安装插件
+     * @param name 插件名称
+     * @param plugin 插件
+     * @param rootNodeName 插件容器名
+     */
+    static install(easy3d, rootNodeName) {
+        PluginBase.easy3DAPI = easy3d;
+        return easy3d.PluginManager.addPlugin(this.pluginName, this, rootNodeName);
+    }
+}
+PluginBase.easy3DAPI = window && window['easy3d'];
+
+
+/***/ })
+
+/******/ });
+
+
+cyjh_soft === undefined && console.error('esm-webpack-plugin: nothing exported!');
+const _cyjh_soft$PluginBase = cyjh_soft['PluginBase'];
+
+export {
+    _cyjh_soft$PluginBase as PluginBase
+}
+//# sourceMappingURL=plugin_base.es.js.map
